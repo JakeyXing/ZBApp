0044f99ef22302a3122f6835de5c49efd304ec43
xingjiehai <xingjiehai@xingjiehaideiMac.local>
2018-10-25 17:15:32 +0800

    .

---
+++ ZBApp/Controller/Mission/TaskStatusViewController.swift
@@ -0,0 +1,145 @@
+//
+//  TaskStatusViewController.swift
+//  ZBApp
+//
+//  Created by   xingjiehai on 2018/10/24.
+//  Copyright © 2018 ParentsChat. All rights reserved.
+//
+
+import UIKit
+import Masonry
+import BEMCheckBox
+import MJRefresh
+import MBProgressHUD
+import Toast
+
+private let  kMissionCellID = "kMissionCellID"
+class TaskStatusViewController: UIViewController {
+
+    var statusType: String!
+    lazy var taskList = [ZB_Task]()
+    var pagendex = 1
+    
+    private lazy var topBarBg: UIView = {
+        let view = UIView(frame: CGRect.init(x: 0, y: 0, width: DEVICE_WIDTH, height: kResizedPoint(pt: 40)))
+        return view
+    }()
+    
+    private lazy var typeDropdownView: JHDropdownView = {
+        let view = JHDropdownView(frame: CGRect.init())
+        view.contentLabel.text = "全部类型"
+        view.contentLabel.textColor = kFontColorBlack
+        view.contentLabel.font = kFont(size: 15)
+        view.dataArray = ["全部类型","摆场","撤场","维修","清扫"]
+        return view
+    }()
+    
+    lazy var timeButton: UIButton = {
+        let btn = UIButton(type: UIButton.ButtonType.custom)
+        btn.setTitleColor(kFontColorGray, for: UIControl.State.normal)
+        btn.titleLabel?.font = kFont(size: 15)
+        btn.setTitle("任务日期", for: .normal)
+        btn.addTarget(self, action: #selector(timeAction), for: UIControl.Event.touchUpInside)
+        return btn
+    }()
+    
+    lazy var checkBox: BEMCheckBox = {
+        let box = BEMCheckBox()
+        box.boxType = BEMBoxType.square
+        box.onFillColor = kTintColorYellow
+        box.onTintColor = kTintColorYellow
+        box.onCheckColor = UIColor.white
+        return box
+    }()
+    
+    private lazy var onlyuselessLabel: UILabel = UILabel.cz_label(withText: "仅无效订单", fontSize: kResizedFont(ft: 15), color: kFontColorGray)
+    
+    
+    var tableview:UITableView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor .white
+        self.navigationController?.navigationBar.isHidden = true
+        self.view.addSubview(self.topBarBg)
+        
+        self.topBarBg.addSubview(self.typeDropdownView)
+        self.topBarBg.addSubview(self.timeButton)
+        self.topBarBg.addSubview(self.onlyuselessLabel)
+        self.topBarBg.addSubview(self.checkBox)
+        self.setTableView()
+        
+        self.typeDropdownView.mas_makeConstraints { (make:MASConstraintMaker!) in
+            make.left.equalTo()(self.topBarBg.mas_left)?.offset()(kResizedPoint(pt: 30))
+            make.centerY.equalTo()(self.topBarBg.mas_centerY)
+            make.width.equalTo()(kResizedPoint(pt: 90))
+            make.height.equalTo()(kResizedPoint(pt: 28))
+        }
+        
+        self.timeButton.mas_makeConstraints { (make:MASConstraintMaker!) in
+            make.centerY.equalTo()(self.topBarBg.mas_centerY)
+            make.left.equalTo()(self.typeDropdownView.mas_right)?.offset()(kResizedPoint(pt: 15))
+            make.width.equalTo()(kResizedPoint(pt: 90))
+            make.height.equalTo()(kResizedPoint(pt: 30))
+        }
+        
+        self.onlyuselessLabel.mas_makeConstraints { (make:MASConstraintMaker!) in
+            make.right.equalTo()(self.topBarBg.mas_right)?.offset()(kResizedPoint(pt: -30))
+            make.centerY.equalTo()(self.topBarBg.mas_centerY)
+            make.height.equalTo()(kResizedPoint(pt: 17))
+        }
+        
+        self.checkBox.mas_makeConstraints { (make:MASConstraintMaker!) in
+            make.right.equalTo()(self.onlyuselessLabel.mas_left)?.offset()(kResizedPoint(pt: -10))
+            make.centerY.equalTo()(self.topBarBg.mas_centerY)
+            make.width.equalTo()(kResizedPoint(pt: 20))
+            make.height.equalTo()(kResizedPoint(pt: 20))
+        }
+    }
+    
+    func setTableView(){
+        tableview = UITableView(frame: CGRect.init(x: 0, y: self.topBarBg.bottom, width: DEVICE_WIDTH, height: DEVICE_HEIGHT-navigationBarHeight-tabbarHeight-kResizedPoint(pt: 40)), style: UITableView.Style.plain)
+        tableview?.separatorStyle = UITableViewCell.SeparatorStyle.none
+        tableview?.backgroundColor = kBgColorGray_221
+        tableview?.dataSource = self;
+        tableview?.delegate = self;
+        tableview?.register(MissionCell.self, forCellReuseIdentifier: kMissionCellID)
+        self.view.addSubview(tableview!);
+    }
+    
+    //MARK: - actions
+    @objc private func timeAction(){
+        let calendarBg = CalendarBgView(frame: CGRect.init())
+        calendarBg.delegate = self;
+        UIApplication.shared.keyWindow?.addSubview(calendarBg)
+        
+    }
+    
+    
+}
+
+extension TaskStatusViewController:UITableViewDelegate,UITableViewDataSource,CalendarBgViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: kMissionCellID, for: indexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return kResizedPoint(pt: 160)
+    }
+    
+    func calendarDidChoosedDate(choosedDate: Date) {
+        //更新提醒时间文本框
+        let formatter = DateFormatter()
+        //日期样式
+        formatter.dateFormat = "yyyy-MM-dd"
+        print(formatter.string(from: choosedDate))
+        
+        self.timeButton.setTitle(formatter.string(from: choosedDate), for: .normal)
+    }
+    
+}
--- ZBApp/Model/ZB_User.swift
+++ ZBApp/Model/ZB_User.swift
@@ -10,16 +10,27 @@ import UIKit
 import YYModel
 
 class ZB_User: NSObject {
-    @objc var accessToken:String?
-    @objc var refreshToken:String?
+    @objc var accessToken:String?//自己用
+    @objc var refreshToken:String?//自己用
     
-    
-    
-    
+    @objc  var id: Int64 = 0
+    @objc  var isDel: Bool = false
+    @objc var nationality:String?
+    @objc var password:String?
+    @objc var phoneNo:String?
+    @objc var sex:String?
+    @objc var userImgUrl:String?
+    @objc var userLevel:String?
+    @objc var userName:String?
+    @objc var userScore:Int = 0
+    @objc var userStatus:String?
+    @objc var validNo:String?
+    @objc var validNoImgUrl:String?
+    @objc var validType:String?
+    @objc var visaImgUrl:String?
+    @objc var workCity:String?
 
     override var description: String {
         return yy_modelDescription()
     }
-
-
 }
